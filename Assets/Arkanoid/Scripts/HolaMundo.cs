8823d11a03dc1ce24f826f47c2bd6423997a81d6
FereBlanco <fere.blanco@gmail.com>
2025-01-24 09:44:17 +0100

    Base project update

---
+++ Assets/Arkanoid/Scripts/HolaMundo.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Arkanoid.Scripts
+{
+    public class HolaMundo : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Hola mundo!");        
+        }
+    }
+}
