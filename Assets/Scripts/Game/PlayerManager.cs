630573025317a90313c995ea374e69ca3825170a
FereBlanco <fere.blanco@gmail.com>
2025-01-28 14:06:25 +0100

    PlayerManager adds extra lives when certain score is reached

--- Assets/Scripts/Game/PlayerManager.cs
+++ Assets/Scripts/Game/PlayerManager.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 namespace Scripts.Game
@@ -5,15 +6,41 @@ namespace Scripts.Game
     public class PlayerManager : MonoBehaviour
     {
         private int score;
+        private int lives;
+        private int maxLives = 10;
+        private int[] extraLifeScores = new int[] {200, 400, 600};
+        private int extraLifeScoresIndex = 0;
+        private int nextExtraLifeScore;
 
         private void Awake()
         {
             score = 0;
+            lives = 3;
+            nextExtraLifeScore = extraLifeScores[0];
         }
 
         internal int AddScore(int scoreToAdd)
         {
             score += scoreToAdd;
+            if (score >= nextExtraLifeScore)
+            {
+                if (lives < maxLives)
+                {
+                    lives++;
+                    Debug.Log($"Lives: {lives}");
+                }
+
+                if (extraLifeScoresIndex < extraLifeScores.Length)
+                {
+                    nextExtraLifeScore = extraLifeScores[extraLifeScoresIndex];
+                }
+                else
+                {
+                    nextExtraLifeScore += extraLifeScores[extraLifeScores.Length-1];
+                }
+                extraLifeScoresIndex++;
+                Debug.Log($"Next level points: " + nextExtraLifeScore);
+            }
             return score;
         }
     }
